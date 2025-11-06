c63ae1197af795560b6cde785b4be0aee7a0f432
delacruzronalddc <delacruz.ronalddc@gmail.com>
2025-11-06 11:55:33 +0800

    update app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.dcxrhonx.emergencyresponseph',
-  appName: 'EmergencyResponsePH',
+  appName: 'Emergency Response PH',
   webDir: 'dist'
 };
 
